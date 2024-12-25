23fab7ae62f30bf6fc5043c69f039bba00787ce8
Jesal Patel <mail@jesal.dev>
2024-12-25 15:50:52 -0500

    improve rgb matrix colors

--- jesal.c
+++ jesal.c
@@ -30,39 +30,54 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 };
 
 void keyboard_post_init_user(void) {
-    rgb_matrix_mode(RGB_MATRIX_CYCLE_LEFT_RIGHT);
+    rgb_matrix_enable_noeeprom();
+    rgb_matrix_sethsv_noeeprom(255, 255, 255);
+    rgb_matrix_mode_noeeprom(RGB_MATRIX_CYCLE_LEFT_RIGHT);
+    rgb_matrix_set_speed_noeeprom(128);
 }
 
-// Change RGB Matrix color based on layer
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state|default_layer_state)) {
-        case 0:
-            rgb_matrix_mode(RGB_MATRIX_CYCLE_LEFT_RIGHT);
+        case BASE:
+            rgb_matrix_sethsv_noeeprom(255, 255, 255);
+            rgb_matrix_mode_noeeprom(RGB_MATRIX_CYCLE_LEFT_RIGHT);
             break;
-        case 1:
+        case NAV:
             rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
-            rgb_matrix_sethsv_noeeprom(0, 255, 255);  // Red
+            rgb_matrix_sethsv_noeeprom(0, 0, 0);
             break;
-        case 2:
-            rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
-            rgb_matrix_sethsv_noeeprom(85, 255, 255); // Green
+        case SYMBOLS:
+            rgb_matrix_sethsv_noeeprom(255, 255, 255);
+            rgb_matrix_mode_noeeprom(RGB_MATRIX_CYCLE_LEFT_RIGHT);
             break;
-        case 3:
+        case QWERTY:
             rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
             rgb_matrix_sethsv_noeeprom(43, 255, 255); // Yellow
             break;
-        case 4:
+        case GAME:
             rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
-            rgb_matrix_sethsv_noeeprom(170, 255, 255); // Blue
+            rgb_matrix_sethsv_noeeprom(216, 18, 180); // Light Purple
             break;
-        case 5:
+        case NUMPAD:
             rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
             rgb_matrix_sethsv_noeeprom(128, 255, 255); // Cyan
             break;
-        case 6:
+        case SYSTEM:
             rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
             rgb_matrix_sethsv_noeeprom(213, 255, 255); // Purple
             break;
     }
     return state;
+}
+
+bool rgb_matrix_indicators_user(void) {
+    switch (get_highest_layer(layer_state|default_layer_state)) {
+        case NAV:
+            rgb_matrix_set_color(38, 129, 216, 208);
+            rgb_matrix_set_color(39, 129, 216, 208);
+            rgb_matrix_set_color(40, 129, 216, 208);
+            rgb_matrix_set_color(41, 129, 216, 208);
+            break;
+    }
+    return false;
 }
